a4ab3e1fe85ed95053b2077fa82370e7bf562a5d
Boyko Andrey <flanker@flymac.local>
2014-07-30 12:05:07 +0300

    создан каркас проекта добавлены таргеты для ios и mac

---
+++ Common/NetworkHelper.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkHelper.swift
+//  RemoteMouse
+//
+//  Created by Boyko Andrey on 7/30/14.
+//  Copyright (c) 2014 LOL. All rights reserved.
+//
+
+import Foundation
+let BonjourDomain = "";
+
+
+class NetworkHelper: NSObject {
+    func registerServer(){
+        
+    }
+}
---
+++ RemoteMouseMac/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  RemoteMouseMac
+//
+//  Created by Boyko Andrey on 7/30/14.
+//  Copyright (c) 2014 LOL. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
